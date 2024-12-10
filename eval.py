6f17721001b2a5b6a55c7f3973089b20a215bc15
Byeonghu Na <wp03052@kaist.ac.kr>
2024-12-10 15:49:26 +0900

    Debug for visualization

--- eval.py
+++ eval.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     if args.method != "plan":
         model = torch.load(join(args.model_path, f"{args.model_name}.pth"), map_location=device)
         model.device = device
+        model.eps_model.device = device
         model.eval()
 
         gen_paths = model.sample(args.eval_num)
