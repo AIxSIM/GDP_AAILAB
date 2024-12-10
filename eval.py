e6e95d5c2f7c1ddbc85c278f22cd5381ed857278
Byeonghu Na <wp03052@kaist.ac.kr>
2024-12-10 15:45:59 +0900

    Debug for visualization

--- eval.py
+++ eval.py
@@ -31,8 +31,7 @@ if __name__ == "__main__":
         print(f"vertex: {n_vertex}")
 
     if args.method != "plan":
-        model = torch.load(join(args.model_path, f"{args.model_name}.pth"))
-        model = model.to(device)
+        model = torch.load(join(args.model_path, f"{args.model_name}.pth"), map_location=device)
         model.eval()
 
         gen_paths = model.sample(args.eval_num)
