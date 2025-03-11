745ba03cdf321ac59a4150c16bdf6cccf45a17fd
Byeonghu Na <wp03052@kaist.ac.kr>
2025-03-12 08:49:59 +0900

    save

--- eval.py
+++ eval.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         gen_paths = model.sample(args.eval_num)
         real_paths = dataset.get_real_paths(args.eval_num)
 
-        torch.save(gen_paths, join(args.model_path, "gen_paths.pth"))
+        torch.save(gen_paths, join(args.model_path, f"{args.model_name}_gen_paths.pth"))
         evaluator = Evaluator(real_paths, gen_paths, model, n_vertex, dataset=dataset,
                               name=join(args.res_path, f"{args.model_name}_pure_gen"), sim_time=args.sim_time)
         evaluator.eval(suffix=args.d_name)
