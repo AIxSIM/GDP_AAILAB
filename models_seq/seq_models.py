367155fa78014b649b057cd06bdfcd98e14c3be3
Byeonghu Na <wp03052@kaist.ac.kr>
2025-12-10 13:46:18 +0900

    kjwj exceptional debug

--- models_seq/seq_models.py
+++ models_seq/seq_models.py
@@ -358,8 +358,8 @@ class Restorer(nn.Module):
                 removal["edges"].append([edge[0], edge[1]])
                 removal["edges_reverse"].append([edge[1], edge[0]])
 
+        self.A = (self.A != 0).to(self.A.dtype)
         new_A = self.A.clone().detach()
-        new_A = (new_A != 0).to(new_A.dtype)
 
         if "nodes" in removal.keys():
             for node in removal["nodes"]:
