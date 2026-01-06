c07565e5fe795ff0154ce34f5ca08d0eddd307ed
Byeonghu Na <wp03052@kaist.ac.kr>
2026-01-06 11:29:30 +0900

    disc

--- models_seq/seq_models.py
+++ models_seq/seq_models.py
@@ -207,8 +207,6 @@ class Restorer(nn.Module):
                 Et_minus_one_bar_hat_x0 = self.matrices[ts - 1] @ x0_pred_probs.transpose(2, 1)
                 Et_minus_one_bar_hat_x0 = rearrange(Et_minus_one_bar_hat_x0, "b c h -> (b h) c")
                 pred_probs_unorm = EtXt * Et_minus_one_bar_hat_x0
-                import pdb
-                pdb.set_trace()
                 sum_probs = torch.clamp(pred_probs_unorm.sum(1, keepdim=True), min=1e-8)
                 pred_probs = pred_probs_unorm / sum_probs
                 mask = (sum_probs == 1e-8)[:, 0]
@@ -482,8 +480,8 @@ class Restorer(nn.Module):
                 pred_probs = pred_probs * guidance[..., :V_pred]
                 pred_probs = pred_probs.view(B * H, V_pred)
 
-                sum_probs = torch.clamp(pred_probs_unorm.sum(1, keepdim=True), min=1e-8)
-                pred_probs = pred_probs_unorm / sum_probs
+                sum_probs = torch.clamp(pred_probs.sum(1, keepdim=True), min=1e-8)
+                pred_probs = pred_probs / sum_probs
                 mask = (sum_probs == 1e-8)[:, 0]
                 pred_probs[mask] = 1.0 / pred_probs.shape[1]
                 #########################
