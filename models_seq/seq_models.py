7a1259091e771f0f366752d087a63f2a5ebdd83a
Byeonghu Na <wp03052@kaist.ac.kr>
2026-01-17 20:00:30 +0900

    debug

--- models_seq/seq_models.py
+++ models_seq/seq_models.py
@@ -423,7 +423,7 @@ class Restorer(nn.Module):
                             g = torch.autograd.grad(logP.sum(), x_in, create_graph=False)[0]  # [B,H,V]
                         v_cur = xt_padded.unsqueeze(-1)  # [B,H,1]
                         g_cur = torch.gather(g, dim=-1, index=v_cur)  # [B,H,1]
-                        logP_tilde = logP[:, None, None] - (g - g_cur)  # [B,H,V]
+                        logP_tilde = logP[:, None, None] + (g - g_cur)  # [B,H,V]
                         P_tilde_clamped = torch.exp(logP_tilde).clamp(min=1e-6, max=1 - 1e-6)
                         log_odds = torch.log(P_tilde_clamped) - torch.log1p(-P_tilde_clamped)
                         guidance = torch.exp(self.args.guidance_scale * log_odds)
