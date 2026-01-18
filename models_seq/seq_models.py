bbd8024dce054d5271db8868cac0725326045afe
Byeonghu Na <wp03052@kaist.ac.kr>
2026-01-18 16:45:23 +0900

    debug

--- models_seq/seq_models.py
+++ models_seq/seq_models.py
@@ -451,10 +451,8 @@ class Restorer(nn.Module):
                         logP_tilde = logP[:, None, None] + (g - g_cur)  # [B,H,V]
                         P_tilde_clamped = torch.exp(logP_tilde).clamp(min=1e-6, max=1 - 1e-6)
                         log_odds = torch.log(P_tilde_clamped) - torch.log1p(-P_tilde_clamped)
-                        weight = self.args.guidance_scale * self.destroyer.betas[1] / self.destroyer.betas
-                        import pdb
-                        pdb.set_trace()
-                        guidance = torch.exp(weight[ts-1][0].item() * log_odds)
+                        weight = self.args.guidance_scale * self.destroyer.betas[0] / self.destroyer.betas
+                        guidance = torch.exp(weight[ts-1][:, None, None] * log_odds)
                         # guidance = torch.exp(log_odds)
                         disc.zero_grad()
 
